7d50f20c260cc8c0b372d313e774a458aa75367b
CoffeeMovedCoder <tiagodinis33@gmail.com>
2022-09-05 21:48:23 +0000

    feat: add loading spinner

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,2 +1,14 @@
 pub mod login;
 pub mod authstatus;
+use spinners::*;
+use colored::Colorize;
+pub fn check_token() {
+    let mut validate_spinner = Spinner::new(Spinners::Dots12, "Checking token".into());
+    validate_spinner.stop_with_message(
+        if crate::auth::validate_token() {
+            format!("{} {}", "✔".green().bold(), "Your token is valid!")
+        } else {
+            format!("{} {}", "!".yellow().bold(), "Your token is invalid!")
+        }
+    );
+}
