5296ef58ef829de7975132f8ce6240e642d60fff
CoffeeMovedCoder <tiagodinis33@gmail.com>
2022-09-06 10:24:05 +0000

    refactor: add log, err and warn functions to maintain consistency across the cli

--- src/commands/login.rs
+++ src/commands/login.rs
@@ -1,12 +1,11 @@
 use clap::*;
-use colored::Colorize;
 pub fn login(matches: &ArgMatches) -> std::io::Result<()>{
     let token = matches.get_one::<String>("token").unwrap().clone();
     if let Err(err) = crate::auth::login(token) {
-        eprintln!("{} Couldn't save the token: {}", "✘".red(), err.kind().to_string());
+        super::err(format!("Couldn't save the token: {}", err.kind().to_string()).as_str());
         Err(err)
     } else {
-        println!("{} Token saved successfully", "✔".green());
+        super::log("Token saved successfully");
         super::check_token();
         Ok(())
     }
