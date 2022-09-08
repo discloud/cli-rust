564348a94319f4f1861d627c25b87f08cc003492
Tiago Dinis <92828847+coffee-is-power@users.noreply.github.com>
2022-09-08 17:43:45 +0100

    feat: add logs command (#23)

---
+++ src/commands/logs.rs
@@ -0,0 +1,24 @@
+use spinners::{Spinner, Spinners};
+
+pub fn logs(){
+    let token = super::expect_token();
+    match super::ask_for_app(token.clone(), "show the logs") {
+        Ok(app_id) => {
+            let mut spinner = Spinner::new(Spinners::Bounce, "Downloading the logs".into());
+            match crate::entities::app::App::get_logs(token.clone(), app_id) {
+                Ok(logs) => {
+                    spinner.stop_with_message(logs);
+                }
+                Err(err) => {
+                    super::err(&format!("Couldn't delete your app: {}", err));
+                    std::process::exit(1);
+                }
+            }
+            
+        }
+        Err(err) => {
+            super::err(&format!("Couldn't fetch apps from api: {}", err));
+            std::process::exit(1);
+        }
+    }
+}
--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,3 +1,4 @@
+pub mod logs;
 pub mod stop;
 pub mod start;
 pub mod restart;
