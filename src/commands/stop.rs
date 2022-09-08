9ec743eb6846fd724fd614a6250dd912bcdb87fd
Tiago Dinis <92828847+coffee-is-power@users.noreply.github.com>
2022-09-08 13:31:52 +0100

    feat: add stop command (#15)

---
+++ src/commands/stop.rs
@@ -0,0 +1,24 @@
+use spinners::*;
+pub fn stop() {
+    let token = super::expect_token();
+    match super::ask_for_app(token.clone(), "shutdown") {
+        Ok(app_id) => {
+            let mut spinner = Spinner::new(Spinners::Pong, "Shutting down your app".into());
+            match crate::entities::app::App::stop(token.clone(), app_id) {
+                Ok(()) => {
+                    spinner.stop_with_message(super::format_log("Your app is down!"));
+                }
+                Err(err) => {
+                    super::err(&format!("Couldn't shutdown your app: {}", err));
+                    std::process::exit(1);
+                }
+            }
+            
+        }
+        Err(err) => {
+            super::err(&format!("Couldn't fetch apps from api: {}", err));
+            std::process::exit(1);
+        }
+    }
+    
+}
